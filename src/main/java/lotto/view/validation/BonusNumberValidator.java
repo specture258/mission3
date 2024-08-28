2a15ee9c66ce63c4610be353fbbdc36c242ea9e0
specture258 <specture258@hanyang.ac.kr>
2024-08-29 01:32:39 +0900

    Feat: BonusNumberValidator 클래스
    보너스 번호를 입력 받아서 검증하는 로직

--- src/main/java/lotto/view/validation/BonusNumberValidator.java
+++ src/main/java/lotto/view/validation/BonusNumberValidator.java
@@ -1,4 +1,25 @@
 package lotto.view.validation;
 
-public class BonusNumberValidator {
+public class BonusNumberValidator implements Validator {
+    @Override
+    public boolean validate(String input) {
+        validateInteger(input);
+        validateBlank(input);
+        return true;
+    }
+
+    void validateInteger(String value){
+        boolean isDigit = value.chars()
+                .allMatch(Character::isDigit);
+        if(!isDigit){
+            Validator.throwError("보너스 번호가 숫자가 아닙니다");
+        }
+    }
+
+    void validateBlank(String value){
+        final String BLANK = "";
+        if(BLANK.equals(value.trim())){
+            Validator.throwError("보너스 번호가 없습니다");
+        }
+    }
 }
