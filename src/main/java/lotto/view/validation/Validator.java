93621da499367ac845ed85fd2c596ca4793f6f62
specture258 <specture258@hanyang.ac.kr>
2024-08-29 00:18:24 +0900

    Refector: validate 메서드 리턴값 수정(void->boolean)
    while문 안에서 true를 반환하기 위해서

--- src/main/java/lotto/view/validation/Validator.java
+++ src/main/java/lotto/view/validation/Validator.java
@@ -1,8 +1,8 @@
 package lotto.view.validation;
 
-public abstract class Validator {
+public interface Validator {
 
-    public abstract boolean validate(String input);
+    boolean validate(String input);
 
     static void throwError(String message){
         throw new IllegalArgumentException("[ERROR]" + message);
