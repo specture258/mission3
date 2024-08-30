4c1d3f7c0d8ac1d64069209bec08ce5ef9d37de1
specture258 <specture258@hanyang.ac.kr>
2024-08-31 00:01:55 +0900

    Feat: WinningNumberInputView 클래스
    당첨 번호를 입력받고 검증하는 클래스

---
+++ src/main/java/lotto/view/WinningNumberInputView.java
@@ -0,0 +1,33 @@
+package lotto.view;
+
+import lotto.view.validation.Validator;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class WinningNumberInputView {
+    public Validator winningNumberValidator;
+
+    public WinningNumberInputView(Validator winningNumberValidator) {
+        this.winningNumberValidator = winningNumberValidator;
+    }
+
+    public String inputWinningNumber(){
+        String winningNumber = "";
+        boolean validInput = false;
+        while(!validInput){
+            winningNumber = readLine();
+            validInput = handleInputWinningNumberValidation(winningNumber);
+        }
+        return winningNumber;
+    }
+
+    public boolean handleInputWinningNumberValidation(String winningNumber){
+        try{
+            winningNumberValidator.validate(winningNumber);
+            return true;
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+}
