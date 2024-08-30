9d8622178f69e333b2cc76b5f395de87a9986ae6
specture258 <specture258@hanyang.ac.kr>
2024-08-31 01:24:30 +0900

    Feat: Price 클래스
    PriceInputView로부터 입력받고 당첨 금액을 리턴 받는 클래스

--- src/main/java/lotto/model/Price.java
+++ src/main/java/lotto/model/Price.java
@@ -1,4 +1,23 @@
 package lotto.model;
 
+import lotto.view.PriceNumberInputView;
+
 public class Price {
+
+    int price;
+
+    private final PriceNumberInputView priceNumberInputView;
+
+    public Price(PriceNumberInputView priceNumberInputView) {
+        this.priceNumberInputView = priceNumberInputView;
+    }
+
+    public void createPrice(){
+        String input = priceNumberInputView.inputPrice();
+        this.price = Integer.parseInt(input);
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
