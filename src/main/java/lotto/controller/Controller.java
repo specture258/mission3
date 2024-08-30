cb3a940211f1bee15995253ab90c994476258637
specture258 <specture258@hanyang.ac.kr>
2024-08-31 01:33:31 +0900

    Feat: Controller 클래스
    게임의 전반적인 흐름 구현

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,4 +1,59 @@
 package lotto.controller;
 
+import lotto.model.Lotto;
+import lotto.model.Service;
+import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
+
+    public OutputView outputView;
+    public Service service;
+    public Lotto lotto;
+
+    public Controller(OutputView outputView, Service service) {
+        this.outputView = outputView;
+        this.service = service;
+    }
+
+    public void run(){
+        buyLotto();
+        chooseNumber();
+        showResult();
+    }
+
+    private void buyLotto() {
+        outputView.viewPrice();
+        service.getLotto();
+
+        outputView.viewLottoCount(service);
+        List<Lotto> lotteries = service.createLotteries();
+        for(Lotto lottery : lotteries){
+            outputView.viewLotteries(lottery);
+        }
+    }
+
+    private void chooseNumber() {
+        outputView.viewWinningNumbers();
+        service.getWinningNumbers();
+
+        outputView.viewBonusNumber();
+        service.getBonusNumber();
+    }
+
+    private void showResult() {
+        outputView.viewWinningStatistic();
+        service.correspondWithLotto();
+        outputView.viewFifth(service);
+        outputView.viewFourth(service);
+        outputView.viewThird(service);
+        outputView.viewSecond(service);
+        outputView.viewFirst(service);
+        outputView.viewProfit(service);
+
+    }
+
+
 }
