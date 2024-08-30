0819e4921ac89c2ae8d5417c54357772f9a23268
specture258 <specture258@hanyang.ac.kr>
2024-08-31 01:30:01 +0900

    Feat: BonusNumber 클래스
    보너스 번호를 입력받고 번호를 리턴하는 클래스

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -1,4 +1,24 @@
 package lotto.model;
 
+import lotto.view.BonusNumberInputView;
+
 public class BonusNumber {
+
+    int number;
+
+    private final BonusNumberInputView bonusNumberInputView;
+
+    public BonusNumber(BonusNumberInputView bonusNumberInputView) {
+        this.bonusNumberInputView = bonusNumberInputView;
+    }
+
+    public int createBonusNumber(){
+        String input = bonusNumberInputView.inputBonusNumber();
+        this.number = Integer.parseInt(input);
+        return number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
 }
