1ea1f9718d1943b1529a3caf7fe2625d11aa2677
specture258 <specture258@hanyang.ac.kr>
2024-08-31 00:03:57 +0900

    Feat: BonusNumberInputView 클래스
    보너스 번호를 입력받고 검증하는 클래스

---
+++ src/main/java/lotto/view/BonusNumberInputView.java
@@ -0,0 +1,34 @@
+package lotto.view;
+
+import lotto.view.validation.Validator;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class BonusNumberInputView {
+
+    public Validator bonusNumberValidator;
+
+    public BonusNumberInputView(Validator bonusNumberValidator) {
+        this.bonusNumberValidator = bonusNumberValidator;
+    }
+
+    public String inputBonusNumber(){
+        String bonusNumber = "";
+        boolean validInput = false;
+        while(!validInput){
+            bonusNumber = readLine();
+            validInput = handleInputBonusValidation(bonusNumber);
+        }
+        return bonusNumber;
+    }
+
+    public boolean handleInputBonusValidation(String bonusNumber){
+        try{
+            bonusNumberValidator.validate(bonusNumber);
+            return true;
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+}
