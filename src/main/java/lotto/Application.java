d474570d3c9543c3d9aefba01725cdc52edede34
specture258 <specture258@hanyang.ac.kr>
2024-08-31 01:34:43 +0900

    Feat: Controller 클래스 생성하여 게임 시작
    Service와 Outview에 의존하는 Controller 객체 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.controller.Controller;
+import lotto.model.BonusNumber;
+import lotto.model.Lotto;
+import lotto.model.Service;
+import lotto.model.WinningNumbers;
+import lotto.view.BonusNumberInputView;
+import lotto.view.OutputView;
+import lotto.view.WinningNumberInputView;
+import lotto.view.validation.BonusNumberValidator;
+import lotto.view.validation.WinningNumbersValidator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
+
+        Controller controller = new Controller(new OutputView(), new Service());
+        controller.run();
         // TODO: 프로그램 구현
     }
 }
