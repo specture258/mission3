1544a413ce2dea2794482f8ceaf3a8dd7d9c7d15
specture258 <specture258@hanyang.ac.kr>
2024-08-31 02:18:23 +0900

    Feat: validateRange 매서드
    당첨 번호의 중복을 확인하는 매서드

--- src/main/java/lotto/view/validation/WinningNumbersValidator.java
+++ src/main/java/lotto/view/validation/WinningNumbersValidator.java
@@ -9,6 +9,7 @@ public class WinningNumbersValidator implements Validator {
         validateSize(inputList);
         validateInteger(inputList);
         validateRange(inputList);
+        validateDuplicateNumber(inputList);
     }
 
     List<String> StringtoList(String value) {
@@ -18,14 +19,14 @@ public class WinningNumbersValidator implements Validator {
 
     private void validateSize(List<String> numbersList) throws IllegalArgumentException{
         if(numbersList.size() != 6){
-            Validator.throwError("당첨 번호의 개수가 잘못되었습니다");
+            Validator.throwError("당첨 번호의 개수가 잘못되었습니다.");
         }
     }
 
     private void validateInteger(List<String> numbersList) throws IllegalArgumentException{
         for(String number : numbersList){
             if(!Character.isDigit(number.charAt(0))){
-                Validator.throwError("당첨 번호가 숫자가 아닙니다");
+                Validator.throwError("당첨 번호가 숫자가 아닙니다.");
             }
         }
     }
@@ -36,8 +37,18 @@ public class WinningNumbersValidator implements Validator {
                 .toList();
         for (Integer integer : integerList) {
             if (integer < 1 || integer > 45) {
-                Validator.throwError("당첨 번호는 1부터 45사이의 숫자여야 합니다");
+                Validator.throwError("당첨 번호는 1부터 45사이의 숫자여야 합니다.");
             }
+        }
+    }
+
+    private void validateDuplicateNumber(List<String> inputList) throws IllegalArgumentException {
+        List<Integer> stringList = inputList.stream()
+                .map(Integer::parseInt)
+                .distinct()
+                .toList();
+        if(stringList.size() != inputList.size()){
+            Validator.throwError("당첨 번호는 중복될 수 없습니다.");
         }
     }
 }
