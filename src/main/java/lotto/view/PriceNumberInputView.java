d27158dded3f34e1c824fe193e7f965009f4b359
specture258 <specture258@hanyang.ac.kr>
2024-08-31 00:01:22 +0900

    Feat: PriceNumberInputView 클래스
    구입 금액을 입력받고 검증하는 역할

---
+++ src/main/java/lotto/view/PriceNumberInputView.java
@@ -0,0 +1,34 @@
+package lotto.view;
+
+import lotto.view.validation.Validator;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class PriceNumberInputView {
+
+    public Validator priceValidator;
+
+    public PriceNumberInputView(Validator priceValidator) {
+        this.priceValidator = priceValidator;
+    }
+
+    public String inputPrice(){
+        String price = "";
+        boolean validInput = false;
+        while(!validInput){
+            price = readLine();
+            validInput = handleInputPriceValidation(price);
+        }
+        return price;
+    }
+
+    public boolean handleInputPriceValidation(String price){
+        try{
+            priceValidator.validate(price);
+            return true;
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+}
