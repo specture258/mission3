5651aa9ab38d5fbbf6fa68404bc51685d70db937
specture258 <specture258@hanyang.ac.kr>
2024-08-31 01:25:21 +0900

    Feat: WinningNumbers
    당첨 번호를 입력받아서 List로 변환하는 역할을 하는 클래스

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -1,4 +1,34 @@
 package lotto.model;
 
+import lotto.view.WinningNumberInputView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class WinningNumbers {
+    List<Integer> winningNumber = new ArrayList<>();
+
+    private final WinningNumberInputView winningNumberInputView;
+
+    public WinningNumbers(WinningNumberInputView winningNumberInputView) {
+        this.winningNumberInputView = winningNumberInputView;
+
+    }
+
+    public List<Integer> createWinningNumber(){
+        String input = winningNumberInputView.inputWinningNumber();
+        stringToIntegerList(input);
+        return winningNumber;
+    }
+
+    public void stringToIntegerList(String input) {
+        int[] array = Arrays.stream(input.split(","))
+                .mapToInt(Integer::parseInt).toArray();
+
+        winningNumber = Arrays.stream(array)
+                .boxed()
+                .collect(Collectors.toList());
+    }
 }
