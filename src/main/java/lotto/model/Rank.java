94b133b1ccea501ffdbd25e39dda3d2e80d2f055
specture258 <specture258@hanyang.ac.kr>
2024-08-31 01:23:03 +0900

    Feat: Rank 클래스
    등수의 상금을 상수로 가지는 Enum 생성

---
+++ src/main/java/lotto/model/Rank.java
@@ -0,0 +1,19 @@
+package lotto.model;
+
+public enum Rank {
+    FIFTH(5_000),
+    FOURTH(50_000),
+    THIRD(1_5000_000),
+    SECOND(30_000_000),
+    FIRST(2_000_000_000);
+
+    private final int money;
+
+    Rank(int money){
+        this.money = money;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+}
